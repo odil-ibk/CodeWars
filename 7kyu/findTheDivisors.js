49779b19984e10d9a6233edb191a7efc6bad84fc
odil-ibk <odil.ibk@gmail.com>
2026-07-23 23:14:06 +0200

    Solving Katas for 23.07.2026

---
+++ 7kyu/findTheDivisors.js
@@ -0,0 +1,19 @@
+/*
+Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#, empty table in COBOL) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+Examples:
+
+divisors(12) --> [2, 3, 4, 6]
+divisors(25) --> [5]
+divisors(13) --> "13 is prime"
+
+*/
+function divisors(int) {
+  let res = [];
+  for (let i = 2; i < int; i++) {
+    if (int % i === 0) {
+      res.push(i);
+    }
+  }
+  return res.length === 0 ? `${int} is prime` : res;
+}
+console.log(divisors(5));
